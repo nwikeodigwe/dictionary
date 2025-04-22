06bf17dd426c7111729fc576648699b8c373468e
Nwike Odigwe <nwikeodigwe@outlook.com>
2025-04-22 07:43:29 +0100

    Added en route

--- app/src/routes/en.ts
+++ app/src/routes/en.ts
@@ -1,11 +1,43 @@
 import express, { RequestHandler } from "express";
+import { dictionary } from "../utils/Dictionary";
+import { status } from "http-status";
 
 const router = express.Router();
 
-const index: RequestHandler = (req, res) => {
-  res.json({ message: "Hello World" });
+const index: RequestHandler = async (req, res) => {
+  const lang = "en";
+  const word = dictionary.getRandomWord(lang);
+  console.log(word);
+  if (!word) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error getting word" });
+    return;
+  }
+  const data = await dictionary.transform(word, lang);
+  if (!data) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error transforming word" });
+    return;
+  }
+  res.status(status.OK).json({ message: status[status.OK], data });
+};
+
+const getWord: RequestHandler = async (req, res) => {
+  const lang = "en";
+  const word = req.params.word;
+  const data = await dictionary.transform(word, lang);
+  if (!data) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error transforming word" });
+    return;
+  }
+  res.status(status.OK).json({ message: status[status.OK], data });
 };
 
 router.get("/", index);
+router.get("/:word", getWord);
 
 export default router;
